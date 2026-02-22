8eb78ea835a1aa517c142e6105027409c4afde93
ErantiDinesh <erantidinesh32@gmail.com>
2026-02-22 19:41:01 +0530

    fix: updated app.js file and db config

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  console.log(process.env.DB_CONNECTION_SECRET);
-  await mongoose.connect(process.env.DB_CONNECTION_SECRET);
+  // console.log(process.env.DB_CONNECTION_SECRET);
+  await mongoose.connect('mongodb+srv://erantidinesh32:Dinnu789@cluster0.rauua.mongodb.net/devtinder');
 };
 
 module.exports = connectDB;
