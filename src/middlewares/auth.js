67122f5145b990c953b5cb9452d9c9e9bd0339ea
ErantiDinesh <erantidinesh32@gmail.com>
2026-02-22 19:52:19 +0530

    fix: updated cookie token

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -8,7 +8,7 @@ const userAuth = async (req, res, next) => {
       return res.status(401).send("Please Login!");
     }
 
-    const decodedObj = await jwt.verify(token, 'DevTinder');
+    const decodedObj = await jwt.verify(token, 'DevTinder@790');
 
     const { _id } = decodedObj;
 
