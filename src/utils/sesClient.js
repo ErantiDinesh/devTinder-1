55f1a74a414970918491e495e88b20f5b4a988c0
ErantiDinesh <erantidinesh32@gmail.com>
2026-03-01 14:24:16 +0530

    fix: updated ses config

--- src/utils/sesClient.js
+++ src/utils/sesClient.js
@@ -1,9 +1,11 @@
 const { SESClient } = require("@aws-sdk/client-ses");
-// Set the AWS Region.
-const REGION = process.env.AWS_REGION;
-// Create SES service object.
-const sesClient = new SESClient({ region: REGION,
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_KEY
- });
-export { sesClient }; 
+
+const sesClient = new SESClient({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_KEY,
+  },
+});
+
+module.exports = { sesClient };
